3c4ac1b33ad554dd1f2c2d3eb8652935e96c8343
PetarPaunov <petarp.2000@abv.bg>
2022-10-20 20:59:02 +0300

    Register is working

---
+++ Source/SkiShop/Controllers/AccountController.cs
@@ -0,0 +1,62 @@
+﻿namespace SkiShop.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using SkiShop.Models.AccountViewModels;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using SkiShop.Data.Models;
+
+    public class AccountController : BaseController
+    {
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public AccountController(SignInManager<ApplicationUser> _signInManager, 
+                                    UserManager<ApplicationUser> _userManager)
+        {
+            signInManager = _signInManager;
+            userManager = _userManager;
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Register()
+        {
+            var model = new RegisterViewModel();
+
+            return View(model);
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = new ApplicationUser()
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+            };
+
+            var result = await userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                await signInManager.SignInAsync(user, isPersistent: false);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(String.Empty, error.Description);
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ Source/SkiShop/Models/AccountViewModels/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SkiShop.Models.AccountViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 5)]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength (50, MinimumLength = 5)]
+        public string Password { get; set; }
+
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+    }
+}
