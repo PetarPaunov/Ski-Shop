a5c7578a1d3f851c6601c7408c660933d1e5e339
PetarPaunov <petarp.2000@abv.bg>
2022-10-21 23:03:29 +0300

    Added brand entity

---
+++ Source/SkiShop.Data/Models/Brand.cs
@@ -0,0 +1,12 @@
+﻿namespace SkiShop.Data.Models
+{
+    public class Brand
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+    }
+}
