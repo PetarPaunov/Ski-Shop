0c8c3a007b3bd02252151119db9c00a896ee102e
PetarPaunov <petarp.2000@abv.bg>
2022-11-16 23:33:43 +0200

    Small changes

--- Source/SkiShop/Extension/SkiShopServiceCollectionExtension.cs
+++ Source/SkiShop/Extension/SkiShopServiceCollectionExtension.cs
@@ -1,18 +1,17 @@
-﻿using SkiShop.Core.Contracts.Admin;
-using SkiShop.Core.Contracts.Common;
-using SkiShop.Core.Contracts.ProductContracts;
-using SkiShop.Core.Contracts;
-using SkiShop.Core.Services.Admin;
-using SkiShop.Core.Services.Common;
-using SkiShop.Core.Services.ProductServices;
-using SkiShop.Core.Services;
-using SkiShop.Data.Common;
-using CloudinaryDotNet;
-using SkiShop.Core.Contracts.ShoppingCart;
-using SkiShop.Core.Services.ShoppingCart;
-
-namespace Microsoft.Extensions.DependencyInjection
+﻿namespace Microsoft.Extensions.DependencyInjection
 {
+    using SkiShop.Core.Contracts;
+    using SkiShop.Core.Contracts.Admin;
+    using SkiShop.Core.Contracts.Common;
+    using SkiShop.Core.Contracts.ProductContracts;
+    using SkiShop.Core.Contracts.ShoppingCart;
+    using SkiShop.Core.Services;
+    using SkiShop.Core.Services.Admin;
+    using SkiShop.Core.Services.Common;
+    using SkiShop.Core.Services.ProductServices;
+    using SkiShop.Core.Services.ShoppingCart;
+    using SkiShop.Data.Common;
+
     public static class SkiShopServiceCollectionExtension
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
