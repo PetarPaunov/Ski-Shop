97b52e990918dcaf8a7306ddb04bab6eaf530988
PetarPaunov <petarp.2000@abv.bg>
2022-12-03 00:16:38 +0200

    Added unit tests

---
+++ Source/SkiShop.Test/AdminServicesTests/RoleServiceAdminTests.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using SkiShop.Core.Contracts.Admin;
+using SkiShop.Core.Services.Admin;
+
+namespace SkiShop.Test.AdminServicesTests
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using SkiShop.Core.Contracts.ProductContracts;
+    using SkiShop.Data.Common;
+    using SkiShop.Data.Models.Account;
+    using SkiShop.Data.Models.Product;
+    using SkiShop.Data.Models.ShoppingCart;
+
+    public class RoleServiceAdminTests
+    {
+        private IServiceProvider serviceProvider;
+        private InMemoryDbContext dbContext;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            dbContext = new InMemoryDbContext();
+            var serviceCollection = new ServiceCollection();
+
+            serviceProvider = serviceCollection
+                .AddSingleton(sp => dbContext.CreateContext())
+                .AddSingleton<IRepository, Repository>()
+                .BuildServiceProvider();
+
+            var repo = serviceProvider.GetService<IRepository>();
+
+            await SeedDbAsync(repo);
+        }
+
+        [Test]
+        public async Task CreateRoleAsync_Should_Add_New_Role_To_The_Database()
+        {
+            var repo = serviceProvider.GetRequiredService<IRepository>();
+
+            var roleStore = new Mock<IRoleStore<IdentityRole>>();
+            Mock<RoleManager<IdentityRole>> mockRoleManager = new Mock<RoleManager<IdentityRole>>
+                (roleStore.Object, null, null, null, null);
+            // mockRoleManager.Setup(x => x.CreateAsync(It.IsAny<IdentityRole>()));
+
+            var mockStore = Mock.Of<IUserStore<ApplicationUser>>();
+            var mockUserManager = new Mock<UserManager<ApplicationUser>>
+                (mockStore, null, null, null, null, null, null, null, null);
+
+            var service = new RoleServiceAdmin(mockRoleManager.Object, repo, mockUserManager.Object);
+
+            await service.CreateRoleAsync("TestRole");
+            await service.CreateRoleAsync("TestRole2");
+
+            var result = await repo.AllReadonly<IdentityRole>().ToListAsync();
+
+            Assert.That(result.Count(), Is.EqualTo(2));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            dbContext.Dispose();
+        }
+
+        private async Task SeedDbAsync(IRepository repo)
+        {
+            var user = new ApplicationUser()
+            {
+                Id = "1",
+                Email = "Test@abv.bg",
+                NormalizedEmail = "Test@abv.bg",
+                UserName = "Test",
+                NormalizedUserName = "TEST",
+            };
+
+            user.ShoppingCart = new ShoppingCart() { UserId = "1" };
+
+            await repo.AddAsync(user);
+           
+            await repo.SaveChangesAsync();
+        }
+    }
+}
---
+++ Source/SkiShop.Test/AdminServicesTests/UserServiceAdminTests.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using SkiShop.Core.Contracts.Admin;
+using SkiShop.Core.Models.UserViewModels;
+using SkiShop.Core.Services.Admin;
+using SkiShop.Data.Common;
+using SkiShop.Data.Models.Account;
+using SkiShop.Data.Models.ShoppingCart;
+
+namespace SkiShop.Test.AdminServicesTests
+{
+    public class UserServiceAdminTests
+    {
+        private IServiceProvider serviceProvider;
+        private InMemoryDbContext dbContext;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            dbContext = new InMemoryDbContext();
+            var serviceCollection = new ServiceCollection();
+
+            serviceProvider = serviceCollection
+                .AddSingleton(sp => dbContext.CreateContext())
+                .AddSingleton<IRepository, Repository>()
+                .BuildServiceProvider();
+
+            var repo = serviceProvider.GetService<IRepository>();
+
+            await SeedDbAsync(repo);
+        }
+
+        [Test]
+        public async Task GetAllUsersAsync_Should_Return_All_Users_In_The_Database2()
+        {
+            var userRoles = new List<string>() { "TestRole" };
+
+            var mockStore = Mock.Of<IUserStore<ApplicationUser>>();
+            var mockUserManager = new Mock<UserManager<ApplicationUser>>
+                (mockStore, null, null, null, null, null, null, null, null);
+
+            mockUserManager.Setup(x => x.GetRolesAsync(It.IsAny<ApplicationUser>()))
+                .ReturnsAsync(userRoles);
+
+
+            var repo = serviceProvider.GetService<IRepository>();
+
+            var service = new UserServiceAdmin(mockUserManager.Object, repo);
+
+            var result = await service.GetAllUsersAsync();
+
+            Assert.That(result.Count(), Is.EqualTo(2));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            dbContext.Dispose();
+        }
+
+        private async Task SeedDbAsync(IRepository repo)
+        {
+            var user = new ApplicationUser()
+            {
+                Id = "1",
+                Email = "Test@abv.bg",
+                NormalizedEmail = "Test@abv.bg",
+                UserName = "Test",
+                NormalizedUserName = "TEST",
+            };
+
+            user.ShoppingCart = new ShoppingCart() { UserId = "1" };
+
+            var user2 = new ApplicationUser()
+            {
+                Id = "2",
+                Email = "Test@abv.bg2",
+                NormalizedEmail = "Test@abv.bg2",
+                UserName = "Test2",
+                NormalizedUserName = "TEST2",
+            };
+
+            user2.ShoppingCart = new ShoppingCart() { UserId = "2" };
+
+            await repo.AddAsync(user);
+            await repo.AddAsync(user2);
+
+            await repo.SaveChangesAsync();
+        }
+    }
+}
